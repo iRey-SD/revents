eeedc8152cd50f6795f9cac1bde54681863069e1
Rawan <ireysd@gmail.com>
2020-06-02 14:13:14 +0300

    the lift column is updated

--- src/features/event/EventDashboard/EventDashBoard.jsx
+++ src/features/event/EventDashboard/EventDashBoard.jsx
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
 import { Grid } from 'semantic-ui-react'; 
+import EvintList from '../EventList/EvintList';
 class EventDashBoard extends Component {
     render() {
         return (
             <Grid>
 
                 <Grid.Column width={10}>
-                    <h2>Left Colmn</h2>
+                    <EvintList/>
                 </Grid.Column>
                 <Grid.Column width={6}>
                     <h2>Right Colmn</h2>
---
+++ src/features/event/EventList/EvintList.jsx
@@ -0,0 +1,16 @@
+import React, { Component , Fragment } from 'react'
+import EventListItem from './EventListItem'
+
+class EvintList extends Component {
+    render() {
+        return (
+            <dFragment>
+                <EventListItem/>
+                <EventListItem/>
+                <EventListItem/>
+            </dFragment>
+        )
+    }
+}
+
+export default EvintList 
